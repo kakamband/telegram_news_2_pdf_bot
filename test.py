14ee3668a114c720d8e4a79ca61ea24674076ea5
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-12-31 14:10:29 -0800

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import news_2_pdf
+import os
+import sys
+
+def test():
+	# news_2_pdf.gen(news_source='bbc')
+	news_2_pdf.gen(news_source='nyt')
+	
+test()

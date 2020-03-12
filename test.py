f4d85ecd3ad85ff158792e4dd873e7521c0f60a1
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-12 02:35:41 -0700

    commit

--- test.py
+++ test.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import news_2_pdf
+import channel2pdf
 import os
 import sys
 
 def test():
 	# news_2_pdf.gen(news_source='bbc')
-	news_2_pdf.gen(news_source='nyt')
+	# news_2_pdf.gen(news_source='nyt')
+	channel2pdf.gen('opinion_feed')
 	
 if __name__ == "__main__":
 	test()

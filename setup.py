e5a560f3d3bdb30a7e2eb37c677e1beb3632b2f7
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-23 14:12:04 -0700

    commit

--- setup.py
+++ setup.py
@@ -4,35 +4,21 @@ import sys
 def kill():
 	os.system("ps aux | grep ython | grep loop_pdf | awk '{print $2}' | xargs kill -9")
 
-def setup(arg = ''):
-	if arg == 'kill':
+def setup():
+	if 'kill' in str(sys.argv):
 		kill()
 		return 
-
-	RUN_COMMAND = 'nohup python3 -u loop_pdf.py &'
 	
-	if arg != 'debug':
-		r = os.system('pip3 install --user -r requirements.txt')
-		if r != 0:
-			os.system('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
-			os.system('python3 get-pip.py')
-			os.system('rm get-pip.py')
-			os.system('pip3 install --user -r requirements.txt')
-		
-	try:
-		from telegram.ext import Updater, MessageHandler, Filters
-	except:
-		os.system('pip3 install python-telegram-bot --upgrade') # need to use some experiement feature, e.g. message filtering
-
+	os.system('pip3 install --user -r requirements.txt --upgrade')
 	kill()
-	if arg.startswith('debug'):
-		os.system(RUN_COMMAND[6:-2])
+
+	if 'debug' in str(sys.argv):
+		os.system('python3 loop_pdf.py')
+	elif 'skip' in str(sys.argv):
+		os.system('nohup python3 loop_pdf.py skip &')
 	else:
-		os.system(RUN_COMMAND)
+		os.system('nohup python3 loop_pdf.py &')
 
 
 if __name__ == '__main__':
-	if len(sys.argv) > 1:
-		setup(sys.argv[1])
-	else:
-		setup('')
+	setup()
